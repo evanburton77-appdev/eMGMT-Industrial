1b0f5d081fd24b75c457256517b208e63a4affc7
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-11 16:12:51 +0000

    added before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_agent!
+  before_action :authenticate_user_or_agent!, unless: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :timezone, :photo, :password])
     devise_parameter_sanitizer.permit(:update, keys: [:email, :first_name, :timezone, :photo, :password])
+  end
+
+  private
+
+  def authenticate_user_or_agent!
+    return if current_user.present? || current_agent.present?
+
+    redirect_to new_user_session_path
   end
 end
