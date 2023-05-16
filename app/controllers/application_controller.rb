4c6b4a56446b656b31dcf2904ebeb3ed5619581d
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-16 16:02:11 +0000

    added id input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :timezone, :photo, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:id, :email, :first_name, :timezone, :photo, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :timezone, :photo, :password])
   end
 
