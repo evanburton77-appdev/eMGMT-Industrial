fe12d8f4f428e5fc96af8e001564403103a5143d
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-17 15:47:35 +0000

    added all info to edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :timezone, :photo, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :timezone, :photo, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :timezone, :photo, :password, :website, :twitter, :instagram, :facebook, :last_name, :phone, :mobile, :address])
   end
 
   private
